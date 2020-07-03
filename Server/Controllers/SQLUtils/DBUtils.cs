9fc00286b0e8af1e9bcef6e88867dfa3c48d0a11
alexkireev15 <alexkireev15@gmail.com>
2020-07-03 20:11:57 +0700

    qwerty

--- Server/Controllers/SQLUtils/DBUtils.cs
+++ Server/Controllers/SQLUtils/DBUtils.cs
@@ -3,18 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace Server.Controllers.SQLUtils
 {
     public class DBUtils
     {
         public static OracleConnection GetDBConnection()
-        {
-            string host = "localhost";
-            int port = 1521;
-            string sid = "orclproj";
-            string user = "c##alexkireev";
-            string password = "tiger";
+        {   
+            
+            string host = WebConfigurationManager.AppSettings.Get("host");
+            int port = Int32.Parse(WebConfigurationManager.AppSettings.Get("port"));
+            string sid = WebConfigurationManager.AppSettings.Get("sid");
+            string user = WebConfigurationManager.AppSettings.Get("user");
+            string password = WebConfigurationManager.AppSettings.Get("password");
 
             return DBOracleUtils.GetDBConnection(host, port, sid, user, password);
         }
